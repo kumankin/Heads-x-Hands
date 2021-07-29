dcaa1670539651aaa77689ed031f2712a147fdb2
Dmitry Kumankin <31110752+kumankin@users.noreply.github.com>
2021-07-29 23:24:48 +0300

    update readme.md

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import create_arrays
 
 def main():
-    print(create_arrays.short(3))
     print(create_arrays.long(3))
+    print(create_arrays.short(5))
+
 if __name__ == "__main__":
     main()   
