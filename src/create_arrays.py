48c8af2db7ccda383782433ecebf992f79cd59d4
Dmitry Kumankin <31110752+kumankin@users.noreply.github.com>
2021-07-29 22:54:01 +0300

    add short and long functions

--- src/create_arrays.py
+++ src/create_arrays.py
@@ -1,4 +1,30 @@
 import random
 
+def long(n):
+    """
+    This function returns an array of n arrays of arbitrary length filled with random numbers,
+    the even subarrays of which are sorted in ascending order, and the odd ones in descending order.
+
+    This is a long version of the function. it reads well.
+
+    >> long(3)
+    [[14, 15, 26, 55, 58, 92], [58], [47, 79, 88]]
+    """
+    # 
+    arrays_len = random.sample(range(1, random.randint(n + 1, 100)), n)
+    arrays = []
+    for i in range(len(arrays_len)):
+        arrays.append(sorted([random.randint(0, 100) for j in range(int(arrays_len[i]))], reverse=bool(i % 2)))
+    return arrays
+
 def short(n):
-    return [sorted([random.randint(0, 100) for i in range(int(random.sample(range(1, random.randint(n + 1, 20)), n)[i]))], reverse=bool(i % 2)) for i in range(int(n))]
+    """
+    This function returns an array of n arrays of arbitrary length filled with random numbers,
+    the even subarrays of which are sorted in ascending order, and the odd ones in descending order.
+
+    This is a short version of the function. It doesn't read well.
+
+    >> short(3)
+    [[14, 15, 26, 55, 58, 92], [58], [47, 79, 88]]
+    """
+    return [sorted([random.randint(0, 100) for i in range(int(random.sample(range(1, random.randint(n + 1, 100)), n)[i]))], reverse=bool(i % 2)) for i in range(int(n))]
--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ import create_arrays
 
 def main():
     print(create_arrays.short(3))
+    print(create_arrays.long(3))
 if __name__ == "__main__":
     main()   
